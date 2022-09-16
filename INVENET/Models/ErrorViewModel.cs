500ae283cf707e952f0f1903ae4dd5ba353f0ba1
MagnusIskander <73327235+MagnusIskander@users.noreply.github.com>
2022-09-16 17:04:36 -0500

    First commit.

---
+++ INVENET/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace INVENET.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
